f72e38c48ed9957e4b21a07e8a4f6fa1775f3f7a
KROHNICK <business.kanekperez@gmail.com>
2019-02-11 15:44:28 -0800

    update fix

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ server.put("/api/users/:id", (req, res) => {
           .json({ message: "The user with the specified ID does not exist." });
       } else {
         db.findById(req.params.id)
-          .then(updatedUser => res.status(200).json(updatedUser))
+          .then(updated => res.status(200).json(updated))
           .catch(err =>
             res.status(404).json({ error: "User not found after updating." })
           );
